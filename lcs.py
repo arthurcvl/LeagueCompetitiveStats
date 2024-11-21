198329ac8737b5b3469a48f26a929139deef1acb
arthur <arthur>
2024-11-21 18:01:54 -0300

    idk

---
+++ lcs.py
@@ -0,0 +1,16 @@
+
+
+FLY = {"name": "FlyQuest"}
+HUNDRET = {"name": "100 Thieves"}
+C9 = {"name": "Cloud9"}
+TL = {"name": "Team Liquid"}
+DIG = {"name": "Dignitas"}
+NRG = {"name": "NRG"}
+SR = {"name": "Shopify Rebellion"}
+IMT = {"name": "Immortals"}
+
+
+
+
+
+LCS = [FLY, HUNDRET, C9, TL, DIG, NRG, SR, IMT]
---
+++ lpl.py
@@ -0,0 +1,24 @@
+
+
+BLG = {"name": "Bilibili Gaming"}
+JDG = {"name": "JD Gaming"}
+TOP = {"name": "Top Esports"}
+FPX = {"name": "Funplus Phoenix"}
+NIP = {"name": "Ninjas in Pyjamas"}
+LNG = {"name": "LNG Esports"}
+WBG = {"name": "Weibo Gaming"}
+OMG = {"name": "OMG"}
+WE = {"name": "Team WE"}
+IG = {"name": "Invictus Gaming"}
+AL = {"name": "Anyone s Legend"}
+LGD = {"name": "LGD Gaming"}
+TT = {"name": "TT"}
+RNG = {"name": "Royal Never Give Up"}
+EDG = {"name": "Edward Gaming"}
+RA = {"name": "Rare Atom"}
+UP = {"name": "Ultra Prime"}
+
+
+
+
+LPL = [BLG, JDG, TOP, FPX, NIP, LNG, WBG, OMG, WE, IG, AL, LGD, TT, RNG, EDG, RA, UP]
