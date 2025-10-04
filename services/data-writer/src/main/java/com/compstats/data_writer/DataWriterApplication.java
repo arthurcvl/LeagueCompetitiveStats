67b32146e55a949948a5a30566ab0d881b1a2b3c
arthurcvl <arthurcvleao@gmail.com>
2025-10-03 21:04:36 -0300

    feat: Create data-writer component SpringBoot archetype

---
+++ services/data-writer/src/main/java/com/compstats/data_writer/DataWriterApplication.java
@@ -0,0 +1,13 @@
+package com.compstats.data_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataWriterApplication.class, args);
+	}
+
+}
---
+++ services/data-writer/src/test/java/com/compstats/data_writer/DataWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compstats.data_writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
