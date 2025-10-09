9260b0a93770b5222b6b3cbef39e49de06ffbbcd
arthurcvl <arthurcvleao@gmail.com>
2025-10-09 17:03:54 -0300

    fix: Change findByName to findByTeamName

--- services/data-api/src/main/java/com/compstats/data_api/team/persistence/TeamRepository.java
+++ services/data-api/src/main/java/com/compstats/data_api/team/persistence/TeamRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, String>, TeamRepositoryCustom {
-    Optional<Team> findByName(String name);
+    Optional<Team> findByTeamName(String name);
 
 }
