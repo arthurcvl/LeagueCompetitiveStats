68cb85abdce2430a7b9ad5bb292219ac31f4a770
arthurcvl <arthurcvleao@gmail.com>
2025-10-09 14:50:13 -0300

    feat: Implement TeamRepositoryCustom methods and link it to TeamRepository

--- services/data-api/src/main/java/com/compstats/data_api/team/persistence/TeamRepository.java
+++ services/data-api/src/main/java/com/compstats/data_api/team/persistence/TeamRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TeamRepository extends JpaRepository<Team, String> {
+public interface TeamRepository extends JpaRepository<Team, String>, TeamRepositoryCustom {
     Optional<Team> findByName(String name);
 
 }
---
+++ services/data-api/src/main/java/com/compstats/data_api/team/persistence/TeamRepositoryCustomImp.java
@@ -0,0 +1,62 @@
+package com.compstats.data_api.team.persistence;
+
+import com.compstats.data_api.team.enums.ComparisonType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class TeamRepositoryCustomImp implements TeamRepositoryCustom {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<String> expectedValues = new ArrayList<>(List.of("kills", "dragons", "towers", "matchDuration"));
+
+    @Override
+    public Double getAverageByTeamNameStatus(String teamName, String statusName) {
+
+        if(!expectedValues.contains(statusName)){
+            throw new IllegalArgumentException("");
+        }
+
+        String jpql = "SELECT ROUND(AVG(" + statusName + "), 2) FROM Team a WHERE a.teamName = :teamNameParam";
+
+        TypedQuery<Double> query = entityManager.createQuery(jpql, Double.class);
+        query.setParameter("teamNameParam", teamName);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Double getRelativeFrequencyByTeamNameStatus(String teamName, String statusName, Integer mark, ComparisonType comparisonType) {
+        String comparisonSignal;
+        if(comparisonType.equals(ComparisonType.GREATER)){
+            comparisonSignal = ">";
+        } else {
+            comparisonSignal = "<";
+        }
+
+        if(isMarkNotValidValue(mark)){
+            throw new IllegalArgumentException();
+        }
+
+        if(!expectedValues.contains(statusName)){
+            throw new IllegalArgumentException("");
+        }
+
+        String jpql = "SELECT CAST((SELECT COUNT(*) FROM Team a WHERE " + statusName + " " + comparisonSignal + " :mark AND a.teamName = :teamNameParam) AS DOUBLE)" + " / (SELECT COUNT(*) FROM Team a WHERE a.teamName = :teamNameParam)";
+
+        TypedQuery<Double> query = entityManager.createQuery(jpql, Double.class);
+        query.setParameter("mark", mark);
+        query.setParameter("teamNameParam", teamName);
+        return query.getSingleResult();
+    }
+
+    private boolean isMarkNotValidValue(Integer mark){
+        if(Objects.isNull(mark) || mark < 0) return true;
+        return false;
+    }
+}
