90d8e14cc126854ac348417b5790abb87bfef371
arthurcvl <arthurcvleao@gmail.com>
2025-10-26 12:41:02 -0300

    fix(data-writer): Change ID type in MatchRepository to reflect change in data models

--- services/data-writer/src/main/java/com/compstats/data_writer/MatchRepository.java
+++ services/data-writer/src/main/java/com/compstats/data_writer/MatchRepository.java
@@ -1,8 +1,7 @@
 package com.compstats.data_writer;
 
 import com.compstats.data_writer.model.Match;
-import com.compstats.data_writer.model.MatchPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MatchRepository extends JpaRepository<Match, MatchPK> {
+public interface MatchRepository extends JpaRepository<Match, String> {
 }
